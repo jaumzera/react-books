56642ce4157aad1e5abcb7fd40d49289f1b47337
Jaumzera <joaomassan@gmail.com>
2018-04-11 22:08:55 -0300

    First commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,24 @@
+export default () => {
+  return [
+    {
+      title: 'Java EE Certification For Beginners',
+      detail: 'A definetly book for Java EE Certification',
+      pages: 250
+    },
+    {
+      title: 'Java 9 For Beginners',
+      detail: 'Core concepts of Java 9',
+      pages: 200
+    },
+    {
+      title: 'Cracking Code Interview',
+      detail: 'For a new career',
+      pages: 500
+    },
+    {
+      title: 'NodeJS for Java Developers',
+      detail: 'Move to NodeJS',
+      pages: 190
+    }
+  ];
+};
